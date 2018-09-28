453962ad2d72eb083332bd7ac06e014d30e5b5db
fu731033719 <15706804793@163.com>
2018-09-28 16:29:05 +0800

    Vue cli 3.0生成项目   添加basicScroll视差滚动效果

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './pages/HelloWorld.vue'
+import  * as basicScroll from 'basicscroll'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  mounted () { 
+    const instance = basicScroll.create({
+        elem: document.querySelector('.box'),
+        from: 'top-top',
+        to: 'bottom-bottom',
+        direct: true,
+        props: {
+            '--turn': {
+              from: '0',
+              to: '1turn'
+            },
+            '--move': {
+              from: '0px',
+              to: '0px'
+            }
+        }
+      })
+      instance.start()
+  }
+}
+</script>
+
+<style lang="scss">
+ #app {
+   height: 3000px;
+   .box{
+      width: 5em;
+      height: 5em;
+      border-radius: 100%; 
+      position: fixed;
+      left: 2em;
+      top: 2em;
+      background: linear-gradient(135deg, #3cdddd, #ff1ac6);
+      transform: translateX(var(--move)) rotate(var(--turn));
+      transition: transform .3s linear;
+      will-change: transform;
+   }
+ }
+</style>

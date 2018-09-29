e6958ccf6a2e14d25f50ff63997b1b139f6b45b7
fu731033719 <15706804793@163.com>
2018-09-29 17:19:02 +0800

    spa-blog home页设计

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,14 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div class="box"></div>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './pages/HelloWorld.vue'
-import  * as basicScroll from 'basicscroll'
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  mounted () { 
-    const instance = basicScroll.create({
-        elem: document.querySelector('.box'),
-        from: 'top-top',
-        to: 'bottom-bottom',
-        direct: true,
-        props: {
-            '--turn': {
-              from: '0',
-              to: '1turn'
-            },
-            '--move': {
-              from: '0px',
-              to: '0px'
-            }
-        }
-      })
-      instance.start()
+<style lang="scss" scoped>
+  #app {
+    background: #1b1d1d;
   }
-}
-</script>
-
-<style lang="scss">
- #app {
-   height: 3000px;
-   .box{
-      width: 5em;
-      height: 5em;
-      border-radius: 100%; 
-      position: fixed;
-      left: 2em;
-      top: 2em;
-      background: linear-gradient(135deg, #3cdddd, #ff1ac6);
-      transform: translateX(var(--move)) rotate(var(--turn));
-      transition: transform .3s linear;
-      will-change: transform;
-   }
- }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <div class="top-info">
+      <p class="top-text">banner is there place</p>
+      <div class="top-text-bg"></div>
+    </div>
+    <div class="home-info"></div>
+  </div>
+</template>
+
+<script>
+import * as basicScroll from 'basicscroll'
+export default {
+  name: 'home',
+  mounted () {
+    const top_text = basicScroll.create({
+        elem: document.querySelector('.top-text'),
+        from: '0',
+        to: '200px',
+        direct: true,
+        props: {
+          '--opacity': {
+              from: 1,
+              to: 0
+            }
+        }
+      })
+      top_text.start()
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .home {
+    width: 100%;
+    background: #1b1d1b;
+    .top-info{
+      height: 600px;
+      background-image: 
+      radial-gradient(farthest-side at 0 0,rgba(52, 178, 136, .5),rgba(52, 178, 136, 0)),
+      radial-gradient(farthest-side at 100% 0,rgba(124,192,89,.8),rgba(124,192,89, 0)),
+      radial-gradient(farthest-side at 0 0,rgba(52, 178, 136, .4),rgba(52, 178, 136, 0));
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: antiquewhite;
+      font-size: 3em;
+      font-family: 'Space-Mono';
+      position: sticky;
+      .top-text{
+        opacity: var(--opacity);
+      }
+    }
+    .home-info {
+      width: 100%;
+      height: 2000px;
+      background: #1b1d1b;
+    }
+  }
+</style>
